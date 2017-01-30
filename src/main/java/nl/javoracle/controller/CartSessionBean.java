1a6c8a3eb076323bd1c43ae19f37821a587a23df
Annan <shalom.annan@sogeti.com>
2017-01-30 14:40:21 +0100

    New feature: product once showed and quanity is increased

--- src/main/java/nl/javoracle/controller/CartSessionBean.java
+++ src/main/java/nl/javoracle/controller/CartSessionBean.java
@@ -2,8 +2,9 @@ package nl.javoracle.controller;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
-import java.util.logging.Level;
+import java.util.Map;
 import java.util.logging.Logger;
 
 import javax.annotation.PostConstruct;
@@ -15,8 +16,6 @@ import javax.persistence.PersistenceContext;
 
 import nl.javoracle.model.Product;
 
-
-
 @Named
 @SessionScoped
 public class CartSessionBean implements Serializable {
@@ -27,7 +26,6 @@ public class CartSessionBean implements Serializable {
 	@PersistenceContext
 	EntityManager entityManager;
 	private ArrayList<Product> shoppingCart;
-	private HashMap<Product, Integer> hashCart;
 	private double totalPrice;
 
 	@Inject
@@ -36,37 +34,10 @@ public class CartSessionBean implements Serializable {
 	@PostConstruct
 	private void initializeBean() {
 		shoppingCart = new ArrayList<Product>();
-		hashCart = new HashMap<Product, Integer>();
 	 }
 
 	public void addToCart(Product x) {
 		shoppingCart.add(x);
-	}
-	
-	public ArrayList<Product> getCart() {
-		return shoppingCart;
-//		ArrayList<Product> groupedCart = new ArrayList<Product>();
-//		logger.log(Level.INFO, String.valueOf("size" + shoppingCart.size()));
-//		contains:
-//		for(Product p : shoppingCart) {
-//			logger.log(Level.INFO, String.valueOf("id" + p.getProductId()));
-//			if(!groupedCart.isEmpty()) {
-//				boolean available = false;
-//				for(Product gp: groupedCart) {
-//					logger.log(Level.INFO, String.valueOf("in"));
-//					if(p.getProductId() == gp.getProductId()) {
-//						logger.log(Level.INFO, String.valueOf("found" + p.getProductId()));
-//						continue contains;
-//					} else {
-//						logger.log(Level.INFO, String.valueOf("not") + p.getProductId());
-//						//groupedCart.add(p); contains error
-//					}		
-//				}	
-//			} else {
-//				groupedCart.add(p);
-//			} 
-//		}
-//		return groupedCart;
 	}
 	
 	public double getTotalPrice() {
@@ -87,61 +58,17 @@ public class CartSessionBean implements Serializable {
 		return "order";
 	}
 	
-	public int getAmountEachProduct(Product product) {
-		int sum = 0;
-		for( Product p : shoppingCart) {
-			if(p.getProductId() == product.getProductId()) {
-				sum++;
-			}
-		}
-		logger.log(Level.INFO, String.valueOf(sum));
-		return sum;
-	}
-	
-	/* All HashMethods */
-	
-	public void addToHashCart(Product product) {
-		logger.log(Level.INFO, String.valueOf("product" + product));
-		Integer key = hashCart.get(product);
-		logger.log(Level.INFO, String.valueOf("what is this" + key));
-		if( key == null) {
-			logger.log(Level.INFO, String.valueOf("hiertoch" + key));
-			hashCart.put(product, 0);
-			logger.log(Level.INFO, String.valueOf("prodyct" + hashCart.get(product)));
-		} else {
-			logger.log(Level.INFO, String.valueOf("hmmm" + key));
-			//hashCart.put(product, hashCart.get(product)+1);
+	public Map<Product, Integer> getCart() {
+		Map<Product, Integer> uniqueSet = new HashMap<Product, Integer>();
+		
+		for (Product product : shoppingCart) {
+			uniqueSet.computeIfAbsent(product, k -> Collections.frequency(shoppingCart, product));
+//			if(uniqueSet.get(product) == null) {
+//				uniqueSet.put(product, Collections.frequency(shoppingCart, product));
+//			}
 		}
 		
-	}
-	
-	public void removeFromHashCart(Product product) {
-		if(hashCart.get(product) != null) {
-			
-		}
+		return uniqueSet;
 	}
 	
 }
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-//		HttpSession session = request.getSession();
-//		shoppingCart = (Cart) session.getAttribute("cart");
-		
-//		
-//		Session session = entityManager.unwrap(Session.class);
-//		SessionFactory sessionFactory = session.getSessionFactory();
-//		Product p = sessionFactory.getCurrentSession().;
-//		for(String c : sessionFactory.getAllClassMetadata().keySet()) {
-//			logger.log(Level.INFO, String.valueOf(sessionFactory.getAllClassMetadata().get(c)));
-//		}
-		
